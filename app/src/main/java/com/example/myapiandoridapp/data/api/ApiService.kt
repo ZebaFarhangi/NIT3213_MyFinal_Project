e25f9e0037af12f22b9f9cac07208ad0776e757c
ZebaFarhangi <ziba2338@gmail.com>
2024-09-15 23:35:58 +1000

    import necessary libraries

--- app/src/main/java/com/example/myapiandoridapp/data/api/ApiService.kt
+++ app/src/main/java/com/example/myapiandoridapp/data/api/ApiService.kt
@@ -1,5 +1,13 @@
 package com.example.myapiandoridapp.data.api
 
+import com.example.myapiandoridapp.data.model.Entity
+import com.example.myapiandoridapp.data.model.LoginRequest
+import com.example.myapiandoridapp.data.model.LoginResponse
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
+
 interface ApiService {
     @POST("/ort/auth")
     suspend fun login(@Body loginRequest: LoginRequest): LoginResponse
