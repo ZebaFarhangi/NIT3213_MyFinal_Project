da604b71ed32b322615c1441ef54f295b21f4be4
ZebaFarhangi <ziba2338@gmail.com>
2024-09-16 02:00:07 +1000

    Add layout for Entity Adapter class

--- app/src/main/java/com/example/myapiandoridapp/ui/dashboard/EntityAdapter.kt
+++ app/src/main/java/com/example/myapiandoridapp/ui/dashboard/EntityAdapter.kt
@@ -5,7 +5,6 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import androidx.room.vo.Entity
 import com.example.myapiandoridapp.data.model.Entity
 
 class EntityAdapter(private val onItemClick: (Entity) -> Unit) :
